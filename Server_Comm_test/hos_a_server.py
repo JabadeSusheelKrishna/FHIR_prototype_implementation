ddc222e5da8bc4d1f853f4335b2be0b64450e795
JabadeSusheelKrishna <susheelkrishna95@gmail.com>
2024-05-14 15:04:05 +0530

    added modifications like port number

--- Server_Comm_test/hos_a_server.py
+++ Server_Comm_test/hos_a_server.py
@@ -2,98 +2,95 @@
 This code is to implement the central server
 '''
 
+Hospital_port_number = 5051     # Provide Port where you want Hospital server to be run
+FHIR_port_number = 8000         # Provide Port or url where Docker is running
+fhir_url = "http://localhost:" + str(FHIR_port_number) + "/fhir/"
+
 from flask import Flask, jsonify, request
-import time
 import requests
-import pprint
 import hashlib
 from flask_cors import CORS
-import json
-import time
 
 Hash_data = {}
 
 app = Flask(__name__)
 CORS(app)
-url = "http://localhost:8000/fhir/"
+url = fhir_url
 
 def generate_hash_id(first_name, last_name, dob):
+    '''
+    geneates hash_id with first name, last name and Date of Birth
+    '''
     date, year, month = dob.split("-")
-    
     input_string = f"{first_name.lower()}{last_name.lower()}{date}{month}{year}"
     
-    hash_object = hashlib.sha256(input_string.encode())
-    hash_id = hash_object.hexdigest()
+    hash_object = hashlib.sha256(input_string.encode())     # generating Hash
+    hash_id = hash_object.hexdigest()                       # Converting to 16
     
-    return hash_id
+    return hash_id                                          # returning the Hash
 
 @app.route('/give-me-hash', methods=['GET'])
 def retrive_consent():
+    '''
+    Takes HTTP requests, Extracts the arguments and Generates the Hash.
+    '''
     fname = request.args.get('fname')
     lname = request.args.get('lname')
     dob = request.args.get('dob')
     
     data = generate_hash_id(first_name=fname, last_name=lname, dob=dob)
-    print("Data : ", data)
     return data
 
 @app.route('/store-hash', methods=['GET'])
-def store_in_json():
-    print("Got These for me : ")
+def store_in_dict():
+    '''
+    Stores the data in the Dictionary
+    '''
     name = request.args.get('name')
     hash = request.args.get('hash')
     
-    Hash_data[hash] = name
+    Hash_data[hash] = name      # Storing the information in dictionary
     
-    print("Added Data into Json too")
     return "Added"
-
-@app.route('/store-hash-in-json', methods=['POST'])
-def store_hasher():
-    if request.method == 'POST':
-        data = request.get_json()
-        print("Received hash:", data['hash'])
-        print("Received fullname:", data['fullname'])
-            
-        Hash_data[data['hash']] = data['fullname']
-        
-        return "Hash stored successfully", 200
-    else:
-        return "Only POST requests are allowed", 405
     
 @app.route('/print', methods=['GET'])
 def print_Hash():
+    '''
+    returns the data present in dictionary as a JSON. (Helpful while debugging)
+    '''
     return jsonify(Hash_data)
     
 def get_original_name(name):
+    '''
+    Searches the Hash with our Hash_Data Dictionary and Sends the Name of the Patient
+    '''
     for each_hash in Hash_data:
-        print(">>>> Hashes : ", each_hash)
         if(name == each_hash):
             return Hash_data[each_hash]
     return "Not Found"
 
 @app.route('/patient-details', methods=['GET'])
 def patient_details():
-    name = request.args.get('name')
-    print("--------- Request Came Successfully ----------")
-    print(name)
-    print("++++++++++++++++++++++++++++++++++++++++++++++")
+    '''
+    Get Data request is received here. and Data is sent if it is present
+    it searches the data in our dictionary. if present, then returns the
+    name of the patient and we search the same name in FHIR server. and
+    return the response to Central server again. ("_")
+    '''
+    hash_from_central_server = request.args.get('name')
     
-    o_name = get_original_name(name)
-    print("Patient Name : ", o_name)
-    print("::::::::::::::::::::::::::::::::::::::::::::::")
+    o_name = get_original_name(hash_from_central_server)    # Here we search the Hash and return the Name
     
     if(o_name == "Not Found"):
-        return o_name
+        return o_name       # Case where data doesn't exist in Hospital Server or Has no consent
     
-    print("hospital server")
     complete_url = url + "Patient?given=" + o_name + "&_include=*&_count=5&_pretty=true"
 
     payload = {}
     headers = {}
     
     list_of_patients = {}
-    response = requests.request("GET", complete_url, headers=headers, data=payload)
+    response = requests.request("GET", complete_url, headers=headers, data=payload) # Searching in FHIR
     if(response.json()["total"] > 0):
         list_of_patients = response.json()["entry"]
     else:
@@ -101,4 +98,4 @@ def patient_details():
     return jsonify(list_of_patients)
 
 if __name__ == '__main__':
-    app.run(port=5051, host='0.0.0.0', debug=True)
+    app.run(port=Hospital_port_number, host='0.0.0.0', debug=True)
--- Server_Comm_test/hos_b_server.py
+++ Server_Comm_test/hos_b_server.py
@@ -1,124 +1,101 @@
 '''
-This code implements the hospital server. PLease make sure the Necessary servers are running.
-also have a Look at Line 19 and Line 104.
-    > line 19 is the url where the FHIR local server of Hospital is ran
-    > Line 104 is the port number where hospital server is gonna run
+This code is to implement the central server
 '''
 
+Hospital_port_number = 5052     # Provide Port where you want Hospital server to be run
+FHIR_port_number = 8001         # Provide Port or url where Docker is running
+fhir_url = "http://localhost:" + str(FHIR_port_number) + "/fhir/"
+
 from flask import Flask, jsonify, request
-import time
 import requests
-import pprint
 import hashlib
 from flask_cors import CORS
-import json
-import time
 
 Hash_data = {}
 
 app = Flask(__name__)
 CORS(app)
-url = "http://localhost:8001/fhir/"
+url = fhir_url
 
 def generate_hash_id(first_name, last_name, dob):
     '''
-    Generates a unique hash ID with first_name, last_name and Date of Birth
+    geneates hash_id with first name, last name and Date of Birth
     '''
     date, year, month = dob.split("-")
-    
     input_string = f"{first_name.lower()}{last_name.lower()}{date}{month}{year}"
     
-    hash_object = hashlib.sha256(input_string.encode())
-    hash_id = hash_object.hexdigest()
+    hash_object = hashlib.sha256(input_string.encode())     # generating Hash
+    hash_id = hash_object.hexdigest()                       # Converting to 16
     
-    return hash_id
+    return hash_id                                          # returning the Hash
 
 @app.route('/give-me-hash', methods=['GET'])
 def retrive_consent():
     '''
-    gives you Hash ID when HTTP GET request is recieved to /give-me-hash
+    Takes HTTP requests, Extracts the arguments and Generates the Hash.
     '''
     fname = request.args.get('fname')
     lname = request.args.get('lname')
     dob = request.args.get('dob')
     
-    data = generate_hash_id(first_name=fname, last_name=lname, dob=dob)     # returns 16 charecter Hash ID
-    print("Data : ", data)
+    data = generate_hash_id(first_name=fname, last_name=lname, dob=dob)
     return data
 
 @app.route('/store-hash', methods=['GET'])
-def store_in_json():
+def store_in_dict():
     '''
-    Stores the {hash : name} in the Hash_Data dictionary
+    Stores the data in the Dictionary
     '''
-    print("Got These for me : ")
     name = request.args.get('name')
     hash = request.args.get('hash')
     
-    Hash_data[hash] = name              # { hash_id : name , hash_id , name }
+    Hash_data[hash] = name      # Storing the information in dictionary
     
-    print("Added Data into Json too")
     return "Added"
-
-@app.route('/store-hash-in-json', methods=['POST'])
-def store_hasher():
-    '''
-    Not used for now
-    '''
-    if request.method == 'POST':
-        data = request.get_json()
-        print("Received hash:", data['hash'])
-        print("Received fullname:", data['fullname'])
-            
-        Hash_data[data['hash']] = data['fullname']
-        
-        return "Hash stored successfully", 200
-    else:
-        return "Only POST requests are allowed", 405
     
 @app.route('/print', methods=['GET'])
 def print_Hash():
     '''
-    Prints the data in Hash_data. ( Helpful if while debugging )
+    returns the data present in dictionary as a JSON. (Helpful while debugging)
     '''
     return jsonify(Hash_data)
     
 def get_original_name(name):
     '''
-    retirves the Name of the patient when Hash ID is passed as input argument
+    Searches the Hash with our Hash_Data Dictionary and Sends the Name of the Patient
     '''
     for each_hash in Hash_data:
-        print(">>>> Hashes : ", each_hash)
         if(name == each_hash):
-            return Hash_data[each_hash]     # returning the name
-    return "Not Found"                      # if not present then return Not Found
+            return Hash_data[each_hash]
+    return "Not Found"
 
 @app.route('/patient-details', methods=['GET'])
 def patient_details():
-    name = request.args.get('name')
-    print("--------- Request Came Successfully ----------")
-    print(name)
-    print("++++++++++++++++++++++++++++++++++++++++++++++")
+    '''
+    Get Data request is received here. and Data is sent if it is present
+    it searches the data in our dictionary. if present, then returns the
+    name of the patient and we search the same name in FHIR server. and
+    return the response to Central server again. ("_")
+    '''
+    hash_from_central_server = request.args.get('name')
     
-    o_name = get_original_name(name)
-    print("Patient Name : ", o_name)
-    print("::::::::::::::::::::::::::::::::::::::::::::::")
+    o_name = get_original_name(hash_from_central_server)    # Here we search the Hash and return the Name
     
     if(o_name == "Not Found"):
-        return o_name
+        return o_name       # Case where data doesn't exist in Hospital Server or Has no consent
     
-    print("hospital server")
     complete_url = url + "Patient?given=" + o_name + "&_include=*&_count=5&_pretty=true"
 
     payload = {}
     headers = {}
     
-    response = requests.request("GET", complete_url, headers=headers, data=payload)
+    list_of_patients = {}
+    response = requests.request("GET", complete_url, headers=headers, data=payload) # Searching in FHIR
     if(response.json()["total"] > 0):
-        list_of_patients = response.json()["entry"]         # Data present in FHIR
+        list_of_patients = response.json()["entry"]
     else:
         print("----- No Patient Exists -----")
     return jsonify(list_of_patients)
 
 if __name__ == '__main__':
-    app.run(port=5052, host='0.0.0.0', debug=True)
+    app.run(port=Hospital_port_number, host='0.0.0.0', debug=True)
